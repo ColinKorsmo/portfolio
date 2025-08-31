cc5637f2daf384395a386f8e20d07f72ad79db40
Colin Korsmo <zckorsmo1@buffs.wtamu.edu>
2025-08-31 17:50:01 -0500

    Fixing commits

--- csharp-console-projects/HelloWorld/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-int age = 10;
-var name = "Colin Korsmo";
-Console.WriteLine($"I am {name} and I am {age} years old.");
